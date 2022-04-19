5c736bbc9c8be539d0f68b0123a5848c8408a3f8
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-04-20 02:53:00 +0600

    Weather Apps Second Vedio Documentation Added

--- weather-apps/prepare/google_scrap.py
+++ weather-apps/prepare/google_scrap.py
@@ -11,16 +11,14 @@ def get_weather_data(city):
     session.headers['accept-language'] = LANGUAGE
     response = session.get(url)
     soup = bs(response.text, 'html.parser')
-    # Extract Region
-
+    # Extract Data and Add to Dictionary
     results = {}
     results['region'] = soup.find('div', attrs={'id':'wob_loc'}).text
     results['daytime'] = soup.find('div', attrs={'id':'wob_dts'}).text
     results['weather'] = soup.find('span', attrs={'id':'wob_dc'}).text
     results['temp'] = soup.find('span', attrs={'id':'wob_tm'}).text
-
     # print(results)
-
     return results
 
+# Check Functions
 get_weather_data('New York')
