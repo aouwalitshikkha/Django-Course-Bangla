4627d849ad2f405224538dbbac8d01d487757e50
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-04-19 23:59:12 +0600

    Weather Scrap Function added in Preparation Weather App Folder

---
+++ weather-apps/prepare/google_scrap.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def get_weather_data(city):
+    city = city.replace(' ','+')
+    url = f'https://www.google.com/search?q=weather+of+{city}'
+    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.9"
+    session = requests.Session()
+    session.headers['user-agent'] = USER_AGENT
+    session.headers['accept-language'] = LANGUAGE
+    response = session.get(url)
+    soup = bs(response.text, 'html.parser')
+    # Extract Region
+
+    results = {}
+    results['region'] = soup.find('div', attrs={'id':'wob_loc'}).text
+    results['daytime'] = soup.find('div', attrs={'id':'wob_dts'}).text
+    results['weather'] = soup.find('span', attrs={'id':'wob_dc'}).text
+    results['temp'] = soup.find('span', attrs={'id':'wob_tm'}).text
+
+    # print(results)
+
+    return results
+
+get_weather_data('New York')
