4f9e6ef21891fa486468a62c864c278091a54480
aouwalitshikkha <aouwalitshikkha@gmail.com>
2022-04-20 04:39:08 +0600

    Weather apps preparation complete

--- weather-apps/prepare/django-practice/src/weather/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def home_view(request):
-    # return HttpResponse('<h1>First Django Project</h1>')
-    return render(request, 'home.html')
